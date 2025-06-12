8dad4fc2325d876367c68c8373639dfbb91a8a5b
SS-ROY29 <seanyango29@gmail.com>
2025-06-12 21:24:08 +0800

    Update PageReplacementFIFO.py
    Added a GUI for ease of use

--- PageReplacementFIFO.py
+++ PageReplacementFIFO.py
@@ -1,13 +1,16 @@
 from collections import deque #Imports a double-ended queue for the program
 import random #Imports a random module for randomly generated pages
+from tkinter import * #Imports a GUI to use
 
 #Function That transfers the logical memory to physical memory 
-def fifo_page_replacement(pages, frame_count):
+def fifo_page_replacement(Pages, frame_count, Output):
     queue = deque() #Initialize a queue for the process
     page_faults = 0
+    Output.delete("1.0", END)
+
 
     #Initialize a For loop to determine each page
-    for page in pages:
+    for page in Pages:
         #If the current page is not currently within the queue, it will add an increment to page_fault
         if page not in queue:
             #If the queue is less than the frame_count(3), then the algorithm will add the current page into the queue
@@ -19,12 +22,41 @@ def fifo_page_replacement(pages, frame_count):
                 queue.append(page)
 
             page_faults += 1
-        print(f"Page: {page} -> Frames: {list(queue)}")
+        Output.insert(END, f"Page: {page} -> Frames: {list(queue)}\n")
 
-    print(f"\nTotal Page Faults: {page_faults}")
+    Output.insert(END, f"\nTotal Page Faults: {page_faults}\n")
 
-MaxPages=10 #Maximum number of pages in a logical memory
-pages = [random.randint(1, 9) for frames in range(MaxPages)] #The code will randomly generate 10 numbers in logical memory
+#Function that randomly Generates Number for the pages 
+def GeneratePages(MaxPages, Pages): 
+    
+    for i in range(MaxPages): #The code will randomly generate 10 numbers in logical memory
+        Pages.append(random.randint(0,9))
+    return Pages
+
+#Initialize the GUI
+Window = Tk()
+Window.title("First In First Out")
+Window.geometry("500x500")
+#Frame for the GUI
+frame1 = Frame(Window, width=300, height=300)
+frame1.grid(row=0, column=0)
+
+#Shows the output of the Program
+Output = Text(Window, width= 60, height=20)
+Output.place(relx=0.5, rely=0.6, anchor=CENTER)
+
+#Parameters Used
+MaxPages = 10#Maximum number of pages in a logical memory
 frame_count = 3 #Maximum size of physical address
-print(f"Logical Memory: {pages}")
-fifo_page_replacement(pages, frame_count)#Used to call the function
+Pages = []
+
+#Used to call the function
+GeneratePages(MaxPages, Pages)
+fifo_page_replacement(Pages, frame_count, Output)
+
+#Shows the Randomly Generated Number
+RandomNum = Label(Window, text=f"Pages inside inside the memory: {GeneratePages(MaxPages, Pages)}")
+RandomNum.place(relx=0.5, rely=0.1, anchor=CENTER)
+
+#Start the GUI
+Window.mainloop()
